f28608e3c79b08a7ae27d079e5326532cbed98b6
15639117124 <15639117124@163.com>
2022-01-12 14:12:34 +0800

    第一次命令行提交

---
+++ src/main/java/com/example/servlet1/TestServlet4.java
@@ -0,0 +1,22 @@
+package com.example.servlet1;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+//测试servlet不同的映射方式
+public class TestServlet4 extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("测试servlet的不同映射方式");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req,resp);
+    }
+}
